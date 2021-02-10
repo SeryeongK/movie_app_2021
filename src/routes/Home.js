42b36ac3d7df2c024a203d5e2e2af11d3c1b9849
Serye <99seryoung@naver.com>
2021-02-10 18:31:16 +0900

    [Nomad Coder] movie_app_2021 Done!

---
+++ src/routes/Home.js
@@ -0,0 +1,37 @@
+import React from "react";
+import axios from "axios";
+import Movie from "../components/Movie"
+import "./Home.css";
+
+class Home extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async () => {
+    const {data: {data: {movies}}} = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
+    this.setState({movies, isLoading: false});
+    // this.setState({movies: movies})와 같음, 전자는 state, 후자는 axios
+  }
+  
+  componentDidMount(){
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies} = this.state
+    return (
+      <section className = "container">
+        {isLoading 
+          ? (<div className="loader">
+              <span className="loader__text">Loading</span>
+            </div> 
+          ) : ( <div className="movies">
+          {movies.map(movie => {
+            console.log(movie);
+            return <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres}/>})}
+        </div>)}
+      </section>);
+  }
+}
+
+export default Home;
